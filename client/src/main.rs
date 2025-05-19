9f3be2e8112461a0e32863381307a786f5da585c
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-19 14:33:18 +0200

    Created basic sdl stuff

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,28 @@
+use std::{thread, time::{Duration, Instant}};
+
+use sdl3::render::BlendMode;
+
+const FRAME_TIME: Duration = Duration::from_nanos(166_666_666);
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl3::init().unwrap();
+    
+    let video = sdl.video().unwrap();
+    
+    let mut event_pump = sdl.event_pump().unwrap();
+    
+    let window = video.window("hello", 800, 600).build().unwrap();
+    let mut canvas = window.into_canvas();
+    canvas.set_logical_size(80, 60, sdl3::sys::render::SDL_RendererLogicalPresentation::INTEGER_SCALE).unwrap();
+    canvas.set_blend_mode(BlendMode::None);
+    
+    loop {
+        let start = Instant::now();
+        
+        canvas.present();
+        
+        let elapsed = start.elapsed();
+        let wait = FRAME_TIME.checked_sub(elapsed);
+        thread::sleep(wait.unwrap_or_default());
+    }
 }
