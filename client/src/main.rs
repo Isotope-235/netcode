a4d05232e93f671a30ca2bbb82eafda51650d2c9
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-19 14:39:19 +0200

    Can now be closed

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 use std::{thread, time::{Duration, Instant}};
 
-use sdl3::render::BlendMode;
+use sdl3::{pixels::Color, rect::{Point, Rect}, render::BlendMode};
 
 const FRAME_TIME: Duration = Duration::from_nanos(166_666_666);
 
@@ -16,8 +16,26 @@ fn main() {
     canvas.set_logical_size(80, 60, sdl3::sys::render::SDL_RendererLogicalPresentation::INTEGER_SCALE).unwrap();
     canvas.set_blend_mode(BlendMode::None);
     
-    loop {
+    'game: loop {
         let start = Instant::now();
+        
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        
+        event_pump.pump_events();
+        for event in event_pump.poll_iter() {
+            use sdl3::event::Event as Ev;
+            
+            match event {
+                Ev::Quit { .. } => break 'game,
+                Ev::KeyDown { .. } => {},
+                Ev::MouseButtonDown { x, y, .. } => {
+                    canvas.set_draw_color(Color::WHITE);
+                    canvas.fill_rect(Rect::from_center(Point::new(x as _, y as _), 20, 20)).unwrap();
+                }
+                _ => {}
+            }
+        }
         
         canvas.present();
         
