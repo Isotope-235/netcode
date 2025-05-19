8031e11d41b562356764c074c624be0a83c9c863
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-19 14:58:26 +0200

    Fixed the white square

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,6 +2,10 @@ use std::{thread, time::{Duration, Instant}};
 
 use sdl3::{pixels::Color, rect::{Point, Rect}, render::BlendMode};
 
+const TITLE: &'static str = "netcode";
+const LOGICAL_WIDTH: u32 = 160;
+const LOGICAL_HEIGHT: u32 = 120;
+const SCALE: u32 = 8;
 const FRAME_TIME: Duration = Duration::from_nanos(166_666_666);
 
 fn main() {
@@ -11,9 +15,9 @@ fn main() {
     
     let mut event_pump = sdl.event_pump().unwrap();
     
-    let window = video.window("hello", 800, 600).build().unwrap();
+    let window = video.window(TITLE, LOGICAL_WIDTH * SCALE, LOGICAL_HEIGHT * SCALE).build().unwrap();
     let mut canvas = window.into_canvas();
-    canvas.set_logical_size(80, 60, sdl3::sys::render::SDL_RendererLogicalPresentation::INTEGER_SCALE).unwrap();
+    canvas.set_logical_size(160, 120, sdl3::sys::render::SDL_RendererLogicalPresentation::INTEGER_SCALE).unwrap();
     canvas.set_blend_mode(BlendMode::None);
     
     'game: loop {
@@ -31,7 +35,7 @@ fn main() {
                 Ev::KeyDown { .. } => {},
                 Ev::MouseButtonDown { x, y, .. } => {
                     canvas.set_draw_color(Color::WHITE);
-                    canvas.fill_rect(Rect::from_center(Point::new(x as _, y as _), 20, 20)).unwrap();
+                    canvas.fill_rect(Rect::from_center(Point::new((x as u32 / SCALE) as _, (y as u32 / SCALE) as _), 20, 20)).unwrap();
                 }
                 _ => {}
             }
