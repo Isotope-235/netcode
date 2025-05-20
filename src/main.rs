92ef3359d7e30bb2ae31bb7e7b6a5889a2a20c19
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-20 14:49:43 +0200

    Moved main

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,30 @@
 use std::{
-    error::Error, net::{Ipv4Addr, UdpSocket}, thread, time::{Duration, Instant},
+    error::Error, net::{Ipv4Addr, UdpSocket}, time::Duration,
     ops::{Add, AddAssign, Mul} 
 };
 
 use sdl3::{
-    keyboard::Keycode,
     pixels::Color,
     rect::{Point, Rect},
-    render::{BlendMode, Canvas, FRect},
+    render::{Canvas, FRect},
     video::Window,
 };
 
-mod cfg;
+mod client;
+mod sys;
+mod server;
 mod networking;
 
 const HOST: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
 const PORT: u16 = 56665;
 const SERVER_HOST: Ipv4Addr = HOST;
 const SERVER_PORT: u16 = 7878;
-const TITLE: &str = "netcode";
 const LOGICAL_WIDTH: u32 = 160;
 const LOGICAL_HEIGHT: u32 = 120;
-const SCALE: u32 = 8;
 const FRAME_TIME: Duration = Duration::from_nanos(16_666_666);
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut state = Game {
-        player_idx: Some(0),
-        players: vec![Player {
-            pos: Vec2 {
-                x: (LOGICAL_WIDTH / 2) as _,
-                y: (LOGICAL_HEIGHT / 2) as _,
-            },
-            velocity: Vec2::new(0., 0.),
-            color: Color::RED,
-            size: 10.0,
-        }],
-        platforms: Vec::new(),
-    };
-
-    let sdl = sdl3::init()?;
-
-    let video = sdl.video()?;
-
-    let mut event_pump = sdl.event_pump()?;
-
-    let window = video
-        .window(TITLE, LOGICAL_WIDTH * SCALE, LOGICAL_HEIGHT * SCALE)
-        .build()?;
-    let mut canvas = window.into_canvas();
-    canvas
-        .set_logical_size(
-            LOGICAL_WIDTH,
-            LOGICAL_HEIGHT,
-            sdl3::sys::render::SDL_RendererLogicalPresentation::INTEGER_SCALE,
-        )?;
-    canvas.set_blend_mode(BlendMode::None);
-
-    let mut movement = (0, 0);
-    let client = std::net::UdpSocket::bind((HOST, PORT))?;
-    client.connect((SERVER_HOST, SERVER_PORT))?;
-
-    'game: loop {
-        let start = Instant::now();
-
-        for event in event_pump.poll_iter() {
-            use sdl3::event::Event as Ev;
-
-            match event {
-                Ev::Quit { .. } => break 'game,
-                Ev::KeyDown {
-                    keycode: Some(kc),
-                    repeat: false,
-                    ..
-                } => match kc {
-                    Keycode::A => movement.0 -= 1,
-                    Keycode::D => movement.0 += 1,
-                    Keycode::W => movement.1 -= 1,
-                    Keycode::S => movement.1 += 1,
-                    _ => (),
-                },
-                Ev::KeyUp {
-                    keycode: Some(kc),
-                    repeat: false,
-                    ..
-                } => match kc {
-                    Keycode::A => movement.0 += 1,
-                    Keycode::D => movement.0 -= 1,
-                    Keycode::W => movement.1 += 1,
-                    Keycode::S => movement.1 -= 1,
-                    _ => (),
-                },
-                _ => ()
-            }
-        }
-
-        send(&client, Command { x: movement.0, y: movement.1 });
-        player_movement(&mut state, movement);
-        render(&state, &mut canvas);
-        canvas.present();
-
-        let elapsed = start.elapsed();
-        let wait = FRAME_TIME.checked_sub(elapsed);
-        thread::sleep(wait.unwrap_or_default());
-    }
-    
-    Ok(())
+    client::run()
 }
 
 fn render(game: &Game, canvas: &mut Canvas<Window>) {
