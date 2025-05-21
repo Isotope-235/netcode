f362f042de367f543fc0d265aa246284de45663a
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-21 15:46:08 +0200

    Prediction

--- src/client.rs
+++ src/client.rs
@@ -2,7 +2,7 @@ use std::{error::Error, time::Duration};
 
 use sdl3::keyboard::KeyboardState;
 
-use crate::{Game, render, server, sys};
+use crate::{Game, player_input, player_movement, render, server, simple_player_input, sys};
 
 const HOST: std::net::Ipv4Addr = std::net::Ipv4Addr::new(127, 0, 0, 1);
 const PORT: u16 = 0;
@@ -32,17 +32,23 @@ pub fn run(mut sdl: sys::SdlContext, shared: Game) -> Result<(), Box<dyn Error>>
         send(&client, movement);
 
         let mut buf = [0; 2048];
-        if let Ok(read) = client.recv(&mut buf) {
+        while let Ok(read) = client.recv(&mut buf) {
             println!("got data: {:?}", &buf[..read]);
             state.shared = serde_json::from_slice(&buf[..read]).unwrap();
         }
 
+        predict(&mut state, movement);
         render(&state.shared, &mut sdl.canvas);
 
         tick.wait();
     }
 
     Ok(())
+}
+
+fn predict(state: &mut State, movement: (i8, i8)) {
+    simple_player_input(&mut state.shared, state.player_idx, movement, DELTA_TIME);
+    player_movement(&mut state.shared, DELTA_TIME);
 }
 
 fn get_input(keyboard: KeyboardState<'_>) -> (i8, i8) {
