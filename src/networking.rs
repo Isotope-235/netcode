e385d70fe0e3674bb1df75021f00936679849880
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-22 14:45:55 +0200

    Moved sending thread spawning out of client constructor

--- src/networking.rs
+++ src/networking.rs
@@ -1,41 +1,54 @@
-use std::{io, net, sync::mpsc::TryIter, thread};
+use std::{
+    io, net,
+    sync::mpsc::{self, TryIter},
+    thread,
+    time::Duration,
+};
+
+fn spawn_sender(
+    socket: net::UdpSocket,
+    tx: mpsc::Sender<(usize, [u8; 2048])>,
+    delay: Duration,
+) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        loop {
+            let mut buf = [0; 2048];
+            match socket.recv(&mut buf) {
+                Ok(read) => {
+                    let tx_ref = tx.clone();
+                    thread::spawn(move || {
+                        thread::sleep(delay);
+                        tx_ref.send((read, buf)).unwrap();
+                    });
+                }
+                Err(e) => eprintln!("recv error: {e}"),
+            }
+        }
+    })
+}
 
 pub struct Client {
-    simulated_ping: std::time::Duration,
+    simulated_ping: Duration,
     socket: net::UdpSocket,
     sending_thread: thread::JoinHandle<()>,
-    receiver: std::sync::mpsc::Receiver<(usize, [u8; 2048])>,
+    receiver: mpsc::Receiver<(usize, [u8; 2048])>,
 }
 
 impl Client {
     const HOST: net::Ipv4Addr = net::Ipv4Addr::new(127, 0, 0, 1);
     const PORT: u16 = 0;
 
-    pub fn connect<A>(remote: A, simulated_ping: std::time::Duration) -> io::Result<Client>
+    pub fn connect<A>(remote: A, simulated_ping: Duration) -> io::Result<Client>
     where
         A: net::ToSocketAddrs,
     {
         let socket = net::UdpSocket::bind((Self::HOST, Self::PORT))?;
         socket.connect(remote)?;
 
-        let (tx, receiver) = std::sync::mpsc::channel();
-        let socket_ref = socket.try_clone()?;
+        let (tx, receiver) = mpsc::channel();
 
-        let sending_thread = thread::spawn(move || {
-            loop {
-                let mut buf = [0; 2048];
-                match socket_ref.recv(&mut buf) {
-                    Ok(read) => {
-                        let tx_ref = tx.clone();
-                        thread::spawn(move || {
-                            thread::sleep(simulated_ping / 2);
-                            tx_ref.send((read, buf)).unwrap();
-                        });
-                    }
-                    Err(e) => eprintln!("recv error: {e}"),
-                }
-            }
-        });
+        let socket_ref = socket.try_clone()?;
+        let sending_thread = spawn_sender(socket_ref, tx, simulated_ping / 2);
 
         Ok(Self {
             simulated_ping,
