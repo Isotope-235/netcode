e10074bb9fef776c790c8e186130b8188c02a3ce
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-05-25 22:28:38 +0200

    Deduplicated code in render

--- src/render.rs
+++ src/render.rs
@@ -3,7 +3,7 @@ use sdl2::{
     rect::{Point, Rect},
 };
 
-use crate::Game;
+use crate::{Game, math::Vec2};
 
 const BG: Color = Color::WHITE;
 const PLATFORM: Color = Color::BLACK;
@@ -17,17 +17,17 @@ const PLAYER_COLORS: &[Color] = &[
     Color::CYAN,
 ];
 
+fn centered_rect(pos: Vec2, size: (f64, f64)) -> Rect {
+    Rect::from_center(Point::new(pos.x as _, pos.y as _), size.0 as _, size.1 as _)
+}
+
 pub fn game(game: &Game, canvas: &mut sdl2::render::WindowCanvas) {
     canvas.set_draw_color(BG);
     canvas.clear();
 
     canvas.set_draw_color(PLATFORM);
     for platform in &game.platforms {
-        let r = Rect::from_center(
-            Point::new(platform.pos.x as _, platform.pos.y as _),
-            platform.size.0 as _,
-            platform.size.1 as _,
-        );
+        let r = centered_rect(platform.pos, platform.size);
         let _ = canvas.fill_rect(r);
     }
 
@@ -35,11 +35,7 @@ pub fn game(game: &Game, canvas: &mut sdl2::render::WindowCanvas) {
         assert!(i < PLAYER_COLORS.len(), "Not enough colors :(");
 
         canvas.set_draw_color(PLAYER_COLORS[i]);
-        let r = Rect::from_center(
-            Point::new(player.pos.x as _, player.pos.y as _),
-            player.size as _,
-            player.size as _,
-        );
+        let r = centered_rect(player.pos, (player.size, player.size));
         let _ = canvas.fill_rect(r);
     }
 }
